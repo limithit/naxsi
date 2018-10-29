66504d2aacc9cf212b084d87be947389053a629c
Gandalf <zhibu1991@gmail.com>
2018-10-29 10:42:41 +0800

    Ignore alphabetic case comparison
    Ignore alphabetic case comparison

--- naxsi_src/naxsi_runtime.c
+++ naxsi_src/naxsi_runtime.c
@@ -1504,7 +1504,7 @@ ngx_http_basestr_ruleset_n(ngx_pool_t	*pool,
 	if (ret == 1) {
 	  NX_DEBUG(_debug_basestr_ruleset, NGX_LOG_DEBUG_HTTP, req->connection->log, 0, 
 		   "XX-apply rulematch [%V]=[%V] [rule=%d] (match %d times)", name, value, r[i].rule_id, nb_match); 
-	  if (!ngx_strncmp((name)->data, "content-type", 12) 
+	  if (!ngx_strncasecmp((name)->data, (u_char *)"content-type", 12) 
 	        && strstr((char *)(&(r[i].br->rx)->pattern)->data, "%") != NULL ) {
 	      content_type_filter = 1;
 	  }
